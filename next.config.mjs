c38bf5c7dcebc7a2a0a4b71bd0f7d28d62a2ed3a
pickingname <trintrinir@icloud.com>
2024-04-23 09:44:53 +0700

    nextjs config fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: { unoptimized: true },
-    typescript: {
-        // !! WARN !!
-        // Dangerously allow production builds to successfully complete even if
-        // your project has type errors.
-        // !! WARN !!
-        ignoreBuildErrors: true,
-      },
+  output: 'export',
+  images: { unoptimized: true },
+  typescript: {
+      // !! WARN !!
+      // Dangerously allow production builds to successfully complete even if
+      // your project has type errors.
+      // !! WARN !!
+      ignoreBuildErrors: true,
+    },
 };
+
+export default nextConfig;
